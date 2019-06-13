df17f86488849156b8ec4859a58205944998a18e
carlos <carlos@192.168.1.24>
2019-06-14 01:21:27 +0200

    Añadido Menu Principal

--- src/guardatupassword/GuardaTuPassword.java
+++ src/guardatupassword/GuardaTuPassword.java
@@ -5,6 +5,8 @@
  */
 package guardatupassword;
 
+import java.util.Scanner;
+
 /**
  *
  * @author carlos
@@ -15,7 +17,49 @@ public class GuardaTuPassword {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        
+        int menu=1;
+        int menu2=5;
+        String contra="1234"; //La contraseña por omisión es 1234
+        String chckpss;
+        
+        Scanner sc = new Scanner(System.in);
+        Scanner sca = new Scanner(System.in);
+        Scanner scann = new Scanner(System.in);
+        Scanner scanne = new Scanner(System.in);
+        
+        do
+        {
+            System.out.println("Guarda tu Password");
+            System.out.println("Introduce la contraseña");
+            chckpss=sca.nextLine();
+            if(chckpss.equals(contra))
+            {
+                System.out.println("Guarda tu Password");
+                System.out.println("Elige la operacion: 1. Almacenar Contraseña nueva 2. Listar Contraseñas 3. Cambiar login 4. Salir");
+                menu2=scann.nextInt();
+                switch (menu2) {
+                //Almacenar Contraseña nueva
+                    case 1:
+                        break;
+                //Listar Contraseñas
+                    case 2:
+                        break;
+                    case 3:
+                        System.out.println("Introduzca nueva password para login");
+                        contra=scanne.nextLine();
+                        break;
+                    case 4:
+                        menu=2; break;
+                    default:
+                        System.out.println("Opcion no valida");
+                        break;
+                }
+                
+            }
+            
+            
+        } while(menu==1);
     }
     
 }
